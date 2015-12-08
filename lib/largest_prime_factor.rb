56c8a4c3cd14b119970695ee50f62f93b6ccf82b
codyss <cody.schwarz@gmail.com>
2015-12-08 14:03:45 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,28 @@
 # Enter your procedural solution here!
+
+
+def prime?(num)
+    if num == 1 || num == 0
+        return false
+    elsif num == 2
+        return true
+    else
+        factors = (2..Math.sqrt(num)).to_a.detect {|x| num % x == 0}
+        if factors == nil
+            return true
+        else
+            return false
+        end
+    end
+end
+
+
+def largest_prime_factor(input)
+    (1..Math.sqrt(input)).to_a.reverse.select do |x|
+        if input % x == 0 && prime?(x)
+            return x
+        end
+    end
+end
+
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+
+class LargestPrimeFactor
+    def initialize(input)
+        @num = input
+    end
+    def prime?(num)
+        if num == 1 || num == 0
+            return false
+        elsif num == 2
+            return true
+        else
+            factors = (2..Math.sqrt(num)).to_a.detect {|x| num % x == 0}
+            if factors == nil
+                return true
+            else
+                return false
+            end
+        end
+    end
+    def number
+        (1..Math.sqrt(@num)).to_a.reverse.select do |x|
+            if @num % x == 0 && prime?(x)
+                return x
+            end
+        end
+    end
+end
